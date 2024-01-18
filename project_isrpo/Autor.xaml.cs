610d15fb2264df5b46ef910c5fac00b0d8d66351
Кристина <@DESKTOP-HI5PA33>
2024-01-18 17:15:02 +0300

    создала визуал для окон регистрации и авторизации

---
+++ project_isrpo/Autor.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project_isrpo
+{
+    /// <summary>
+    /// Логика взаимодействия для Autor.xaml
+    /// </summary>
+    public partial class Autor : Window
+    {
+        public Autor()
+        {
+            InitializeComponent();
+        }
+
+        private void EyeBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void create_an_acc(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void log_in(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ project_isrpo/Registr.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project_isrpo
+{
+    /// <summary>
+    /// Логика взаимодействия для Registr.xaml
+    /// </summary>
+    public partial class Registr : Window
+    {
+        public Registr()
+        {
+            InitializeComponent();
+        }
+
+        private void EyeBtn1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EyeBtn2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void create(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void log_in(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

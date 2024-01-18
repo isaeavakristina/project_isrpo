a54ecc8f2ac273b48de11ad200059c6164de35c0
Кристина <@DESKTOP-HI5PA33>
2024-01-18 17:24:44 +0300

    создала классы Users и класс, наследующий DbContext

---
+++ project_isrpo/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_isrpo
+{
+    class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UsersDb;" +
+                "Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;" +
+                "Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+        }
+    }
+}
